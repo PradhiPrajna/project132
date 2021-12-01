12c11ceecd64b9952ce6a7bbdb6c11479a2c976d
PradhiPrajna <93035161+PradhiPrajna@users.noreply.github.com>
2021-12-01 20:54:53 +0530

    Add files via upload

---
+++ livingroom.js
@@ -0,0 +1,37 @@
+img ="";
+status ="";
+function preload()
+{ 
+    img = loadImage("room.jpg");
+}
+
+function setup()
+{
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd',modelLoded);
+    document.getElementById("status").innerHTML = "status_detecting_objects"
+}
+function modelLoded()
+{
+    console.log("modelLoaded");
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+function draw()
+{
+    image(img, 0, 0, 640, 420);
+    fill("#660000");
+    text("tv and ac", 45, 75);
+    noFill();
+    stroke("#660000");
+    rect(30, 60, 450, 350);
+}
+function gotResult(error, results)
+{
+    if(error)
+    {
+        console.log(error);
+    } 
+    console.log(results);
+}
